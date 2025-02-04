1dc6946fa52358acf6fe69f32c3ea4148adee706
programing360 <fhlimon360@gmail.com>
2025-02-04 23:58:32 +0600

    added cash out section

--- js/login.js
+++ js/login.js
@@ -17,7 +17,7 @@ document.getElementById('input-login')
             latestUpdate.classList.remove('hidden')
             // console.log('get the form')
         }
-        else{
+        else {
             alert('Pin Number is incorrect. Please try again!')
         }
 
@@ -31,28 +31,64 @@ document.getElementById('Money-btn')
         addMoneyForm.classList.remove('hidden')
         const latestUpdate = document.getElementById('latest-update')
         latestUpdate.classList.add('hidden')
+        document.getElementById('cash-out-form').classList.add('hidden')
     })
 // btn added end----------------------------------------------------
 // add money start--------------------------------------------------
 document.getElementById('add-money-btn')
     .addEventListener('click', function (event) {
         event.preventDefault()
+
         const getAmount = document.getElementById('get-amount').value
         const pGetAmount = parseFloat(getAmount)
-        console.log(typeof pGetAmount)
+        // console.log(typeof pGetAmount)
         const getPin = document.getElementById('get-pin-number').value
         const currentAmount = document.getElementById('current-amount').innerText
         const pCurrentAmount = parseFloat(currentAmount)
-        if(getPin === '1234'){
+        if (getPin === '1234') {
             const newAmount = pCurrentAmount + pGetAmount
             const rightAmount = document.getElementById('current-amount')
             rightAmount.innerText = newAmount
 
-            
+
         }
-        else{
+        else {
             alert('Something is wrong. Please try again!')
         }
         // console.log('paisi', getAmount, getPin)
     })
 // add money end--------------------------------------------------
+// cash out btn start---------------------------------------------
+document.getElementById('cashout-btn')
+    .addEventListener('click', function () {
+        document.getElementById('Add-money-form').classList.add('hidden')
+        document.getElementById('latest-update').classList.add('hidden')
+        const getAmount = document.getElementById('cash-out-form')
+        getAmount.classList.remove('hidden')
+
+    })
+
+document.getElementById('cash-out-btn')
+    .addEventListener('click', function (event) {
+        event.preventDefault()
+        const getPin = document.getElementById('get-pin-number').value
+        const getAmountOfCashOut = document.getElementById('get-amount').value
+        parseFloat(getAmountOfCashOut)
+        const currentBalance = document.getElementById('current-amount').innerText;
+        if(currentBalance > getAmountOfCashOut){
+            alert('you do not enough money to account!')
+        }
+        if(getPin === '1234'){
+            
+
+            const updateBalance = currentBalance - getAmountOfCashOut
+
+            document.getElementById('current-amount').innerText = updateBalance
+
+        }
+        else{
+            alert('you have some mistake.Please try again!')
+        }
+        
+    })
+// cash out btn end---------------------------------------------
